8eecbce05c864fad25def50bcda7cddb72b6747d
Nikita <35489986+Nikita628@users.noreply.github.com>
2020-11-02 11:57:44 +0400

    changed prod config file

--- thullo-react-app/src/common/config.ts
+++ thullo-react-app/src/common/config.ts
@@ -1,13 +1,5 @@
 const config = {
-    apiUrl: "http://localhost:5000/api",
+    apiUrl: "http://nikitabortnikov-001-site1.ctempurl.com/api",
 };
-
-switch (process.env.NODE_ENV) {
-    case "production":
-        config.apiUrl = "";
-        break;
-    default:
-        break;
-}
 
 export default config;
