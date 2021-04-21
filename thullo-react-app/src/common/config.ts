ddb68284fb78addf883892e188c15ac8bdfb962d
nikitabortnikov <nikita.bortnikov@rokolabs.com>
2021-04-21 11:55:46 +0400

    changed config

--- thullo-react-app/src/common/config.ts
+++ thullo-react-app/src/common/config.ts
@@ -1,5 +1,11 @@
+import { env } from "process";
+
 const config = {
     apiUrl: "http://nikitabortnikov-001-site1.ctempurl.com/api",
 };
+
+if (env.NODE_ENV === 'development') {
+    config.apiUrl = "http://localhost:5000";
+}
 
 export default config;
